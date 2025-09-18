fbe65b130ee1f94dfd529f2f4897c8b735ba643f
Esteban <acevedoesteban999@gmail.com>
2025-09-18 14:39:28 -0400

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Plan Change Label',
+    'version': '1.0.0',
+    'summary': "Changing default addon labels",
+    'description':"Changing default addon labels",
+    'author': 'esteban@ydrsoluciones.com',
+    'website': 'https://github.com/acevedoesteban999/plan_change_label',
+    'category': 'Accounting/Accounting',
+    'depends': [
+        'base',
+        'account',
+    ],
+    "data": [],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
